63c6b6cb0171acd5129058b5facfce945c0a58f4
varannik <varanik@live.com>
2025-05-03 13:40:57 +0330

    fix: remove extra entry points

--- src/test/java/com/voiceassistant/gateway/config/SecurityConfigTest.java
+++ src/test/java/com/voiceassistant/gateway/config/SecurityConfigTest.java
@@ -3,7 +3,6 @@ package com.voiceassistant.gateway.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.web.reactive.server.SecurityWebTestClientConfigurer;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
